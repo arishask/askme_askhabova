11b8a92bf16e8c43a7d17752d61655a5867ef30b
arishask <arina880@yandex.ru>
2024-11-10 23:20:46 +0300

    task2 more finished

--- app/views.py
+++ app/views.py
@@ -11,10 +11,18 @@ QUESTIONS = [
     } for i in range(30)
 ]
 
+ANSWERS = [
+    {
+        'title': f'Title {i}',
+        'id': i,
+        'text': f'This is answer for question #{i}'
+    } for i in range(4)
+]
+
 
 def paginate(objects_list, request, per_page):
     page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(QUESTIONS, per_page)
+    paginator = Paginator(objects_list, per_page)
     page = paginator.page(page_num)
     try:
         page = paginator.page(page_num)
@@ -30,30 +38,29 @@ def paginate(objects_list, request, per_page):
 def index(request):
     return render(
         request, 'index.html',
-        context={'questions': paginate(QUESTIONS, request, 5),
-                 'page_obj': paginate(QUESTIONS, request, 5)})
+        context={'questions': paginate(QUESTIONS, request, 5)})
 
 
 def hot(request):
+    hot_questions = QUESTIONS.copy()
+    hot_questions.reverse()
     return render(
         request, 'hot.html',
-        context={'questions': paginate(QUESTIONS.reverse(), request, 5),
-                 'page_obj': paginate(QUESTIONS.reverse(), request, 5)})
+        context={'questions': paginate(hot_questions, request, 5)})
 
 
 def tag(request):
     return render(
         request, 'tag.html',
-        context={'questions': paginate(QUESTIONS, request, 3),
-                 'page_obj': paginate(QUESTIONS, request, 3)})
+        context={'questions': paginate(QUESTIONS, request, 3)})
 
 
 def question(request, question_id):
     one_question = QUESTIONS[question_id]
     return render(
         request, 'one_question.html',
-        context={'questions': paginate(QUESTIONS[question_id], request, 2),
-                 'page_obj': paginate(QUESTIONS[question_id], request, 2)})
+        context={'answers': paginate(ANSWERS, request, 2),
+                 'question': one_question})
 
 
 def login(request):
